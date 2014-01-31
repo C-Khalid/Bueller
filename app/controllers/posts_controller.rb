e02a6a4cb8fa4efa5b35c4097a16b466c40a3d23
Khalid Alsuwaiyel <mekdif@gmail.com>
2014-01-31 01:55:24 +0000

    The picture bug is fixed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,7 @@ class PostsController < ApplicationController
     getPic(@student)
     
     if @student.save
+      flash[:notice] = " the student was successfully created."
       redirect_to :root
     else
       render 'new'
@@ -30,7 +31,9 @@ class PostsController < ApplicationController
     @student = Student.find(params[:id])
     @student.update(params[:student].permit(:name, :nickname, :email, :imageurl))
     getPic(@student)
-    @student.save
+    if @student.save
+      flash[:notice] = "done"
+    end
     redirect_to :root
   end
   
@@ -44,13 +47,14 @@ class PostsController < ApplicationController
   
   def getPic(student)
     
-    if :imageurl.nil?
-      File.write("/home/action/Bueller/app/assets/images/"+Student.count.to_s+".jpg", open( @student.imageurl ).read, {mode: 'wb'})
-    else
+    if params[:imageurl].nil?
       email_address = @student.email.downcase
       hash = Digest::MD5.hexdigest(email_address)
       File.write("/home/action/Bueller/app/assets/images/"+Student.count.to_s+".jpg", open( "http://www.gravatar.com/avatar/#{hash}"
                                                                                            ).read, {mode: 'wb'})
+    else
+      File.write("/home/action/Bueller/app/assets/images/"+Student.count.to_s+".jpg", open( @student.imageurl ).read, {mode: 'wb'})
+    
     end
     @student.imageurl =Student.count.to_s
   end
