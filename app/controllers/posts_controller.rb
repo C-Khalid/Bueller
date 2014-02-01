c2e9253f148304796da2ddbecf88690a83541af3
Khalid Alsuwaiyel <mekdif@gmail.com>
2014-02-01 18:23:27 +0000

    fix image bug, add simple styling.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,6 @@ require 'open-uri'
 require 'digest/md5'
 
 class PostsController < ApplicationController
-
   
   def new
   end
@@ -13,7 +12,17 @@ class PostsController < ApplicationController
   def create
     
     @student = Student.new(params[:post].permit(:name, :nickname, :email, :imageurl))
-    getPic(@student)
+    if @student.imageurl.empty?
+      email_address = @student.email.downcase
+      hash = Digest::MD5.hexdigest(email_address)
+      File.write("/home/action/Bueller/app/assets/images/"+Student.count.to_s+".jpg", open( "http://www.gravatar.com/avatar/#{hash}"
+                                                                                           ).read, {mode: 'wb'})
+    else
+      
+      File.write("/home/action/Bueller/app/assets/images/"+Student.count.to_s+".jpg", open( @student.imageurl.to_s ).read, {mode: 'wb'})
+    
+    end
+    @student.imageurl =Student.count.to_s
     
     if @student.save
       flash[:notice] = " the student was successfully created."
@@ -29,19 +38,32 @@ class PostsController < ApplicationController
   
   def update
     @student = Student.find(params[:id])
-    @tempImagePath = @student.imageurl
+    @tempImagePath  = @student.imageurl
     @student.update(params[:student].permit(:name, :nickname, :email, :imageurl))
     
     if( @student.imageurl.eql? "Delete")
+      
       @student.imageurl = "default"
+      
     elsif( @student.imageurl.eql?  "Change")
-      getPic(@student)     
+      
+          if params[:new_image_url].empty?
+            email_address = @student.email.downcase
+            hash = Digest::MD5.hexdigest(email_address)
+            File.write("/home/action/Bueller/app/assets/images/"+Student.count.to_s+".jpg", 
+                    open( "http://www.gravatar.com/avatar/#{hash}").read, {mode: 'wb'})
+          else
+            File.write("/home/action/Bueller/app/assets/images/"+Student.count.to_s+".jpg", 
+                    open( params[:new_image_url] ).read, {mode: 'wb'})
+   
+          end
+          @student.imageurl =Student.count.to_s  
     else
       @student.imageurl = @tempImagePath  
     end
-    
+
     @student.save
-    redirect_to :root, :flash => { :notice => "You successfully updated your profile.!" }
+    redirect_to :root, :notice => "You successfully updated your profile. !"
   end
   
   def show
@@ -49,21 +71,7 @@ class PostsController < ApplicationController
   
   private
   def post_params
-    params.require(:post).permit(:name, :nickname, :email, :imageurl)
-  end
-  
-  def getPic(student)
-    
-    if params[:imageurl].nil?
-      email_address = @student.email.downcase
-      hash = Digest::MD5.hexdigest(email_address)
-      File.write("/home/action/Bueller/app/assets/images/"+Student.count.to_s+".jpg", open( "http://www.gravatar.com/avatar/#{hash}"
-                                                                                           ).read, {mode: 'wb'})
-    else
-      File.write("/home/action/Bueller/app/assets/images/"+Student.count.to_s+".jpg", open( @student.imageurl ).read, {mode: 'wb'})
-    
-    end
-    @student.imageurl =Student.count.to_s
+    params.require(:post).permit(:name, :nickname, :email, :imageurl )
   end
   
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < ActiveRecord::Base
-  
+  def new_image_url=(new_url)
+  end
 end
