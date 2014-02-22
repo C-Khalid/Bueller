126ddeb79b65efb33bc1dc76c3bebf89d8258c00
Khalid Alsuwaiyel <mekdif@gmail.com>
2014-02-22 01:08:55 +0000

    prevent student from creating another attendance for the same day

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -5,15 +5,21 @@ class AttendancesController < ApplicationController
  
   def create
   
-    attendance = Attendance.new
-    attendance.attendance = true
-    date = params[:attendance]
-    attendance.attended_on = Date.new date["attended_on(1i)"].to_i, date["attended_on(2i)"].to_i, date["attended_on(3i)"].to_i
-    attendance.seat = params[:attendance][:seat]
-    attendance.now = DateTime.now
-    attendance.student_id = current_user.id
-    attendance.save
+    if Attendance.where(:student_id => current_user.id , :attended_on => Date.today).empty?
+      attendance = Attendance.new
+      attendance.attendance = true
+      date = params[:attendance]
+      attendance.attended_on = Date.new date["attended_on(1i)"].to_i, date["attended_on(2i)"].to_i, date["attended_on(3i)"].to_i
+      attendance.seat = params[:attendance][:seat]
+      attendance.now = DateTime.now
+      attendance.student_id = current_user.id
+      attendance.save
+    else
+      flash[:notice] = "You have already created an attendance for today!"
+    end
+    
     redirect_to :root
+    
   end
   
   private
